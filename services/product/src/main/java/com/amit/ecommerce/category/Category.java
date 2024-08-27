29f87e7479be96c5c0cb21166a275bcfaa7f76c4
AmitTomar805 <amit.s.tomar805@gmail.com>
2024-08-27 03:52:02 -0400

    Committing Product Micro Service

---
+++ services/product/src/main/java/com/amit/ecommerce/category/Category.java
@@ -0,0 +1,23 @@
+package com.amit.ecommerce.category;
+
+import com.amit.ecommerce.product.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
---
+++ services/product/src/main/java/com/amit/ecommerce/product/ProductController.java
@@ -0,0 +1,4 @@
+package com.amit.ecommerce.product;
+
+public class ProductController {
+}
