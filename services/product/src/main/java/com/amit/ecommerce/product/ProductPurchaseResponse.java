b8aef2d33bdcd6b38d8f0a8a560674e16717e2ae
AmitTomar805 <amit.s.tomar805@gmail.com>
2024-08-28 01:56:32 -0400

    Committing Product Microservice

---
+++ services/product/src/main/java/com/amit/ecommerce/product/ProductPurchaseResponse.java
@@ -0,0 +1,12 @@
+package com.amit.ecommerce.product;
+
+import java.math.BigDecimal;
+
+public record ProductPurchaseResponse(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
